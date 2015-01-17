f855b208db12956cc574d83be08e2179f2b4c939
xxwhiterhinoxx <whiterhino0707@gmail.com>
2015-01-17 02:33:33 -0500

    FW-base: Add option disable brightness slider 2/2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3150,6 +3150,12 @@ public final class Settings {
          * @hide
          */
         public static final String KEY_HOME_DOUBLE_TAP_ACTION = "key_home_double_tap_action";
+        
+        /** Whether to show the brightness slider in quick settings panel.
+         *
+         * @hide
+         */
+        public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
 
         /**
          * Action to perform when the menu key is pressed. (Default is 1)
